affa498d51c01cbde3d1bb4c2d8047ddb0ff5ec4
Ahmad Amireh <ahmad@amireh.net>
2016-05-24 22:59:19 +0300

    TinyMCE: remove dead code

--- tinymce.conf.js
+++ tinymce.conf.js
@@ -6,68 +6,28 @@ var config = {
   outputDir: '/srv/http/docs/tinymce',
   assetRoot: path.resolve(__dirname, '../tinymce'),
   tooltipPreviews: false,
+
+  // there are some UID clashes that we still need to solve:
   strict: false,
-  // emittedFileExtension: '.htm',
-  corpus: {
-  },
   stylesheet: path.resolve(__dirname, 'tinymce.less'),
   styleOverrides: {
     'plain-link': '#2276d2',
   },
   layoutOptions: {
     banner: false,
-    // rewrite: {
-    //   '/api.html': '/index.html',
-    // },
-
-    // bannerLinks: [
-    //   {
-    //     text: 'API',
-    //     href: '/api',
-    //   },
-    // ]
   }
 };
 
 var jsPlugin = require('megadoc-plugin-js')({
   id: 'api',
+
   source: [
-    // 'js/tinymce/classes/AddOnManager.js',
-    // 'js/tinymce/classes/Compat.js',
-    // 'js/tinymce/classes/DragDropOverrides.js',
-    // 'js/tinymce/classes/Editor.js',
-    // 'js/tinymce/classes/EditorCommands.js',
-    // 'js/tinymce/classes/EditorManager.js',
-    // 'js/tinymce/classes/EditorObservable.js',
-    // 'js/tinymce/classes/EditorUpload.js',
-    // 'js/tinymce/classes/EnterKey.js',
-    // 'js/tinymce/classes/Env.js',
-    // 'js/tinymce/classes/FocusManager.js',
-    // 'js/tinymce/classes/ForceBlocks.js',
-    // 'js/tinymce/classes/Formatter.js',
-    // 'js/tinymce/classes/InsertContent.js',
-    // 'js/tinymce/classes/InsertList.js',
-    // 'js/tinymce/classes/jquery.tinymce',
-    // 'js/tinymce/classes/LegacyInput.js',
-    // 'js/tinymce/classes/Mode.js',
-    // 'js/tinymce/classes/NodeChange.js',
-    // 'js/tinymce/classes/NotificationManager.js',
-    // 'js/tinymce/classes/Register.js',
-    // 'js/tinymce/classes/SelectionOverrides.js',
-    // 'js/tinymce/classes/Shortcuts.js',
-    // 'js/tinymce/classes/UndoManager.js',
-    // 'js/tinymce/classes/WindowManager.js',
-    // 'js/tinymce/classes/html/*.js',
-    // 'js/tinymce/classes/*.js',
     'js/tinymce/classes/**/*.js',
   ],
-  // source: 'js/tinymce/classes/ui/FloatPanel.js',
-  exclude: [ 'tests' ],
+
   strict: false,
 
   namedReturnTags: false,
-  namespaceDirMap: {
-  },
 
   builtInTypes: [
     'Element',
@@ -82,10 +42,7 @@ var jsPlugin = require('megadoc-plugin-js')({
     'Node',
     'RangeObject',
     'Window',
-    // 'Integer',
     'Mixed',
-    // 'true',
-    // 'false'
   ],
 });
 
@@ -99,24 +56,12 @@ jsPlugin.on('process-tag', function(tag) {
   var lines;
 
   if (tag.type === 'example') {
-    // lines = tag.string.split('\n');
-
-    // tag.typeInfo.name = lines.shift();
     tag.string = '```js\n' + beautify(tag.string, { indent_size: 4 }) + '\n```';
   }
 
   return tag;
 });
 
-config.plugins = [
-  jsPlugin,
-
-  // require('megadoc-plugin-markdown')({
-  //   id: 'articles',
-  //   source: 'README.md'
-  // }),
-
-  require('megadoc-theme-qt')({}),
-];
+config.plugins = [ jsPlugin, require('megadoc-theme-qt')({}) ];
 
 module.exports = config;
