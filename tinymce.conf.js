8fc75bbea0d72d3117fcf4dd7eaa27ff3f14c233
Ahmad Amireh <ahmad@amireh.net>
2016-05-24 23:03:01 +0300

    Commentary for TinyMCE hacks

--- tinymce.conf.js
+++ tinymce.conf.js
@@ -47,6 +47,12 @@ var jsPlugin = require('megadoc-plugin-js')({
 });
 
 jsPlugin.on('preprocess-tag', function(commentNode) {
+  // Some of their union type-strings (like in @param {A|B}) are delimited by /
+  // which is not correct according the Google Closure Compiler spec; they
+  // have to be delimited by |
+  //
+  // So from: @param {A/B} to @param {A|B}
+  //
   if (commentNode.type) {
     commentNode.type = commentNode.type.replace(/(\w)\/(\w)/g, '$1|$2');
   }
@@ -55,6 +61,13 @@ jsPlugin.on('preprocess-tag', function(commentNode) {
 jsPlugin.on('process-tag', function(tag) {
   var lines;
 
+  // Their examples are not indented to be considered code blocks by Markdown
+  // but most of them are just code, so we'll wrap them in a block as such:
+  //
+  //     ```js
+  //     ```
+  //
+  // and cross our fingers:
   if (tag.type === 'example') {
     tag.string = '```js\n' + beautify(tag.string, { indent_size: 4 }) + '\n```';
   }
